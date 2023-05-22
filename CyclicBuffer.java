72783a439c31b0bcc6f373784f685926df9176c0
Roman <ROliynykov@gmail.com>
2023-05-23 02:39:23 +0300

    Forks count method updated - forks caused by delivery delays have been added

--- CyclicBuffer.java
+++ CyclicBuffer.java
@@ -6,7 +6,7 @@ public class CyclicBuffer {
     int branches = 0;
     public int successfulBlockCnt = 0; // total blocks added to the longest chain
 
-    int slotsAfterLastSuccessfulBlock = 0;
+    int slotsAfterLastSuccessfulBlock = PraosForksSimulation.delta; // to make the first block join the longest chain, not a fork
 
     boolean forksPresent = false;
     int orphanedForkLen = 0;
@@ -19,7 +19,7 @@ public class CyclicBuffer {
 
     public int getNumberOfBranchesWithSlotAdvance(int numberOfSlotLeaders) {
 
-        int seenBranches = branches;
+        int seenBranches = (branches == 0 ? 1 : branches);
 
         int nextSlot = (currentSlot + 1) % ( PraosForksSimulation.delta + 1 );
         branches -= buffer[ nextSlot ];
@@ -46,10 +46,15 @@ public class CyclicBuffer {
 
 // Scenario for forks:
 // if there are several multiple slot leaders, each of them extend a separate existing chain
-// or create a new fork if slot leaders exceed the number of existing chains
-        if ( (numberOfSlotLeaders > 0) && (numberOfSlotLeaders > seenBranches) )
-            forksHappened += ( longestChainExtended ? (numberOfSlotLeaders - seenBranches - 1) :
-                                                        numberOfSlotLeaders - seenBranches );
+// or create a new fork if the number of slot leaders exceed the number of the currently existing chains
+        if ( (numberOfSlotLeaders > 0) && (numberOfSlotLeaders >= seenBranches) )
+            forksHappened += ( longestChainExtended ?
+// all branches, including the longest (final) branch have been extended; extra leaders create new forks
+                    (numberOfSlotLeaders - seenBranches) :
+// the longest (final) branch has not been extended, all slot leaders create new forks if they cannot extend the existing ones
+                    (numberOfSlotLeaders - seenBranches + 1 )
+                    );
+
 
 // Counting the length of the current longest orphaned branch
         if ( forksPresent ) {
